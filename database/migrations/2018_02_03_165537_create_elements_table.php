06a54a2c5e9fce270c7b6cac8181ebd815bd0416
alexeykostenko <aleksey.kostenko.dev@gmail.com>
2018-02-03 19:54:34 +0200

    added migrations

---
+++ database/migrations/2018_02_03_165537_create_elements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateElementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('elements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('endpoint_id')->unsigned();
+            $table->string('title', 100);
+            $table->integer('votes')->unsigned()->default(0);
+            $table->string('photo', 500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('elements');
+    }
+}
